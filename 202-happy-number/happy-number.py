6aca4607f3b8c5f3c97850999bad4e19af22e5e2
Vivek Kasturi <ahimamshk@gmail.com>
2026-01-31 09:05:24 -0800

    Time: 0 ms (100.00%) | Memory: 19.4 MB (28.28%) - LeetSync

---
+++ 202-happy-number/happy-number.py
@@ -0,0 +1,16 @@
+class Solution:
+    def sumOfSquares(self, n:int)->int:
+        total=0
+        while n>0:
+            digit=n%10
+            total += digit*digit
+            n//=10
+        return total
+    def isHappy(self, n: int) -> bool:
+        seen=set()
+        while n!=1:
+            if n in seen:
+                return False
+            seen.add(n)
+            n=self.sumOfSquares(n)
+        return True
