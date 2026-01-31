804760d643f78152cf3fad76a576b6dde878bfcc
Vivek Kasturi <ahimamshk@gmail.com>
2026-01-31 09:44:32 -0800

    Time: 11 ms (54.74%) | Memory: 33.9 MB (13.18%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
@@ -1,6 +1,7 @@
 import numpy as np
+from typing import List
 class Solution:
-    def square(self,a)->int:
+    def square(self,a :np.ndarray)->ndarray:
         return a*a
     def sortedSquares(self, nums: List[int]) -> List[int]:
         a=np.array(nums)
