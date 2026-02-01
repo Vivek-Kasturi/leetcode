34d8544bd98baa6fd29110bd1fbd7cb380e762ef
Vivek Kasturi <ahimamshk@gmail.com>
2026-02-01 09:56:53 -0800

    Time: 26 ms (49.90%) | Memory: 30.5 MB (18.22%) - LeetSync

---
+++ 1512-design-underground-system/design-underground-system.py
@@ -0,0 +1,19 @@
+class UndergroundSystem:
+
+    def __init__(self):
+        self.checkInMap = {}  #id->(startstation, time)
+        self.totalMap ={} #(start station, end station)-> [Tatal time, count]
+        
+    def checkIn(self, id: int, startStation: str, t: int) -> None:
+        self.checkInMap[id]=(startStation, t)
+
+    def checkOut(self, id: int, endStation: str, t: int) -> None:
+        startStation, time = self.checkInMap[id]
+        route=(startStation, endStation)
+        if route not in self.totalMap:
+            self.totalMap[route] = [0,0]
+        self.totalMap[route][0]+=t-time
+        self.totalMap[route][1]+=1
+    def getAverageTime(self, startStation: str, endStation: str) -> float:
+        total, count = self.totalMap[(startStation, endStation)]
+        return total/count
