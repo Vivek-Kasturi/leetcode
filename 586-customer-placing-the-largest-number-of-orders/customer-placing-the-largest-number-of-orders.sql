9ecd406c67d39461bbbc243d72290ba3c559a2be
Vivek Kasturi <ahimamshk@gmail.com>
2026-02-02 18:30:04 -0800

    Time: 519 ms (14.14%) | Memory: 0B (100.00%) - LeetSync

--- 586-customer-placing-the-largest-number-of-orders/customer-placing-the-largest-number-of-orders.sql
+++ 586-customer-placing-the-largest-number-of-orders/customer-placing-the-largest-number-of-orders.sql
@@ -1,6 +1,13 @@
 # Write your MySQL query statement below
+#Follow‑up: If multiple customers tie for the maximum
 select customer_number
 from orders
 group by customer_number
-order by count(*) desc
-limit 1
+having count(*) =(
+    select max(order_count)
+    from(
+    select count(*) as order_count
+    from orders
+    group by customer_number
+    )as t
+)
