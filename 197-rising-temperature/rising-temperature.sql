99a06a474e3cb41be45c2b47ce7b34f8a9c9cf2b
Vivek Kasturi <ahimamshk@gmail.com>
2026-02-06 12:27:48 -0800

    Time: 1016 ms (5.00%) | Memory: 0B (100.00%) - LeetSync

---
+++ 197-rising-temperature/rising-temperature.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+select w1.id from Weather w1
+where w1.temperature >(
+    select w2.temperature 
+    from weather w2
+    where w2.recordDate = date_sub(w1.recordDate, interval 1 day)
+)
