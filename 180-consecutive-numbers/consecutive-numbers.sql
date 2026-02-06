68f51de6e8e9ce3fd7dd083d6771528afe438aed
Vivek Kasturi <ahimamshk@gmail.com>
2026-02-06 11:57:21 -0800

    Time: 1396 ms (5.02%) | Memory: 0B (100.00%) - LeetSync

--- 180-consecutive-numbers/consecutive-numbers.sql
+++ 180-consecutive-numbers/consecutive-numbers.sql
@@ -1,8 +1,14 @@
-SELECT DISTINCT l1.num AS ConsecutiveNums
+#using subquery
+SELECT DISTINCT num AS ConsecutiveNums
 FROM Logs l1
-JOIN Logs l2 
-  ON l1.id + 1 = l2.id 
- AND l1.num = l2.num
-JOIN Logs l3 
-  ON l2.id + 1 = l3.id 
- AND l2.num = l3.num;
+WHERE 
+    num = (
+        SELECT l2.num
+        FROM Logs l2
+        WHERE l2.id = l1.id + 1
+    )
+    AND num = (
+        SELECT l3.num
+        FROM Logs l3
+        WHERE l3.id = l1.id + 2
+    );
